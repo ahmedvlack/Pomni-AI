2e53c53a48f188e8a8dd390d9124d602af1a4bfd
afroto1 <afrotoaldgalfo2@gmail.com>
2026-03-26 01:23:47 +0200

    Update هل.js

--- plugins/هل.js
+++ plugins/هل.js
@@ -1,20 +1,36 @@
-import util from 'util'
-import path from 'path'
 let user = a => '@' + a.split('@')[0]
-function handler(m, { groupMetadata, command, conn, text, usedPrefix}) {
-if (!text) throw `*أدخــل الـسـؤال !*`
-let ps = groupMetadata.participants.map(v => v.id)
-let a = ps.getRandom()
-let x = `${pickRandom(['احــتـمـال قـلـيـل' ,'نــعم بـالـتـأكـيد', 'لا أعـتـقـد', 'مــستـحـيــل'])}`
-let l = Math.floor(Math.random() * x.length);
-let top = `*هــل ${text}*
 
-*الــأجــابـه :* ${x}`.trim()
-conn.sendFile (m.reply (top, null, { mentions: [a]}))}
+function handler(m, { groupMetadata, conn, text }) {
+  if (!text) throw `*أدخــل الـسـؤال !*`
+
+  // منشن صاحب الرسالة
+  let sender = m.sender
+
+  // اختيار رد عشوائي
+  let answer = pickRandom([
+    'احــتـمـال قـلـيـل',
+    'نــعم بـالـتـأكـيد',
+    'لا أعـتـقـد',
+    'مــستـحـيــل',
+    'غــالـبـاً نــعــم',
+    'مــمـكـن'
+  ])
+
+  // نسبة مئوية عشوائية
+  let percent = Math.floor(Math.random() * 101) // من 0 لـ 100
+
+  let top = `*هــل ${text}*\n\n*الــأجــابـه :* ${answer}\n*الـنـسـبـة :* ${percent}%`
+
+  conn.reply(m.chat, top, m, { mentions: [sender] })
+}
+
 handler.help = handler.command = ['هل']
 handler.tags = ['fun']
 handler.group = true
 handler.limit = 0
+
 export default handler
+
 function pickRandom(list) {
-return list[Math.floor(Math.random() * list.length)]}
+  return list[Math.floor(Math.random() * list.length)]
+}
